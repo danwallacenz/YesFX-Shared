43d0bf1649f426d76be98abdd1fe72c651f39da3
Dan Wallace <daniel.wallace.nz@gmail.com>
2020-08-20 07:18:51 +1200

    add TODOs

--- Sources/Shared/Shared.swift
+++ Sources/Shared/Shared.swift
@@ -3,25 +3,25 @@ import Foundation
 public struct UpdateDevice: Codable {
   public let id: UUID?
   public let pushToken: String?
-  public let system: Device.System
+  public let system: Device.System // TODO: remove
   public let osVersion: String
-  public let channels: [String]?
+  public let channels: [String]? // TODO: remove channels
 
   public init(id: UUID? = nil,
               pushToken: String? = nil,
-              system: Device.System,
+              system: Device.System, // TODO: remove
               osVersion: String,
-              channels: [String]? = nil) {
+              channels: [String]? = nil) { // TODO: remove
     self.id = id
     self.pushToken = pushToken
     self.system = system
     self.osVersion = osVersion
-    self.channels = channels
+    self.channels = channels // TODO: remove
   }
 }
 
 public struct Device: Codable {
-  public enum System: String, Codable {
+  public enum System: String, Codable { // TODO: remove
     case iOS
     case android
   }
@@ -30,17 +30,17 @@ public struct Device: Codable {
   public let system: System
   public var osVersion: String
   public var pushToken: String?
-  public var channels: [String]
+  public var channels: [String] // TODO: remove channels
 
   public init(id: UUID,
               system: System,
               osVersion: String,
               pushToken: String?,
-              channels: [String]) {
+              channels: [String]) { // TODO: remove
     self.id = id
     self.system = system
     self.osVersion = osVersion
     self.pushToken = pushToken
-    self.channels = channels
+    self.channels = channels // TODO: remove
   }
 }
